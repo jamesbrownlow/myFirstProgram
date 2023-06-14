e636f6bf19d845f1a331731d25d527622f183c12
Dr. J <jbrownlow@avc.edu>
2023-06-13 18:37:06 -0700

    Add files via upload

--- ball.py
+++ ball.py
@@ -1,8 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 13 17:29:38 2023
+Created on Tue Jun 13 17:54:15 2023
 
 @author: jbrownlow
 """
-myName = input('input your name -> ')
-print(f"Hello {myName}")
+#  y = v0*t − 0.5*g*t**2
+
+def height(v0, t):
+    g = 32.8
+    
+    y = v0*t - 0.5*g*t**2
+    return y
+
+t=1
+while t > 0:
+    v0 = input('input velocity: ')
+    v0 = float(v0)
+    t = input('input time, sec: ')
+    t = float(t)
+    y = height(v0,t)
+    
+    print(f'after {t} seconds height = {y}')
+    
+"""
+at time 1.6 ball got to its max height
+which was about 38 feet.  Assuming we throw it
+vertically at initial velocity 50 fps
+"""
+    
